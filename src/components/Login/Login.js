d7aa2e2e1a7595851d27b7ab57fc8e480a06d761
WreetSarker <wreet.sarker@gmail.com>
2021-03-17 11:36:35 +0600

    done simple authentication using google

---
+++ src/components/Login/Login.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app();
+}
+
+
+const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+    const handleGoogleSignIn = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                const credential = result.credential;
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email };
+                setLoggedInUser(signedInUser);
+                history.replace(from);
+                console.log(signedInUser);
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                const credential = error.credential;
+                // ...
+            });
+
+    }
+    return (
+        <div>
+            <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign in</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYp2xpUB7DTePtk_ky2nNdXlqkVFCXy14",
+    authDomain: "burj-al-arab-wreet.firebaseapp.com",
+    projectId: "burj-al-arab-wreet",
+    storageBucket: "burj-al-arab-wreet.appspot.com",
+    messagingSenderId: "384879124783",
+    appId: "1:384879124783:web:3dfac4877ef82c382e41da"
+};
+
+export default firebaseConfig;
